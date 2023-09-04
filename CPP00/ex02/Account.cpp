cbc1c4b0bdedd876dd5051e7c68579c37254bbf2
michael guerg <michaelguerga@gmail.com>
2023-09-04 16:36:23 +0200

    back to where I was

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -3,16 +3,55 @@
 #include <iostream>
 #include "Account.hpp"
 
+int	Account::_nbAccounts = 8;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbWithdrawals = 0;
+int	Account::_totalNbDeposits = 0;
+
 int	Account::getNbAccounts()
 {
-	return (8);
+	return (_nbAccounts);
+}
+
+Account::~Account(void)
+{
+}
+
+Account::Account(int initial_deposit)
+{
+	static int index = 0;
+	
+	_accountIndex = index++;
+	this->makeDeposit(initial_deposit);	
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
+	
+}
+void	Account::_displayTimestamp(void)
+{
+	std::cout << "[20230904_091532] "; 
 }
 
 void	Account::displayAccountsInfos()
 {
-	std::cout << Account::getNbAccounts(); 
+	std::cout << "display Accounts info" << Account::getNbAccounts() << std::endl;
 }
 
-void	displayStatus( void ) const
+void	Account::displayStatus() const
 {
-	
+	std::cout << "display status" << std::endl; 
+}	
+
+void	Account::makeDeposit(int deposit)
+{
+	_amount = deposit;
+	_totalAmount += deposit;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	_amount = withdrawal;
+	_totalAmount -= withdrawal;
+	_totalNbWithdrawals++;
+	return (true);
+}
